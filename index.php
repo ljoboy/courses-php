e3244a39395f66599553f64dc5eb5b29e2d01f32
Dark Angel <jonathanyombo@gmail.com>
2021-02-11 21:49:24 +0200

    [NEW] index.php on public and project route folder

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * FILE index.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 2/8/2021 - 12:01 AM
+ */
+
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * FILE index.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 2/8/2021 - 12:02 AM
+ */
+
+require '../vendor/autoload.php';
+
+
