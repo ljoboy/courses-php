86f34d67a09edbe84e4fda7c606f27ef6c36e7fa
Dark Angel <jonathanyombo@gmail.com>
2021-02-11 22:39:14 +0200

    [UPDATE] adding a method to get the current url and other one to sanitize url

--- routes/Router.php
+++ routes/Router.php
@@ -21,15 +21,37 @@ class Router
 
     /**
      * Router constructor.
-     * @param string $url
      */
-    public function __construct(string $url)
+    public function __construct()
     {
-        $this->url = $url;
+        $this->url = $this->currentUrl();
     }
 
     public function show()
     {
         echo $this->url;
+    }
+
+    /**
+     * Getting current url
+     *
+     * @return string
+     */
+    public function currentUrl(): string
+    {
+        return $this->sanitize($_SERVER['REQUEST_URI']);
+    }
+
+    /**
+     * get Url Without Query String
+     *
+     * @param string $url
+     * @return string
+     */
+    private function sanitize(string $url): string
+    {
+        $urlSanitized = explode('?', $url);
+
+        return is_array($urlSanitized) ? $urlSanitized[0] : $urlSanitized;
     }
 }
