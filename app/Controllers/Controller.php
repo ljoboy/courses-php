b9bf6b1af329de61c206f1a824a469815e1b89ff
Dark Angel <jonathanyombo@gmail.com>
2021-02-12 13:08:53 +0200

    [NEW] Controller class

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * FILE Controller.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 2/12/2021 - 1:02 PM
+ *
+ * @package App\Controllers;
+ */
+
+namespace App\Controllers;
+
+/**
+ * Class Controller
+ * @package App\Controllers
+ */
+class Controller
+{
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+
+    }
+}
