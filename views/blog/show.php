0fc6ab1fa723920a0e0e9ff9405471ff142d0e90
Dark Angel <jonathanyombo@gmail.com>
2021-02-12 14:08:24 +0200

    [NEW] views for testing

---
+++ views/blog/show.php
@@ -0,0 +1,3 @@
+<h1>Post n* <?= $params['id'] ?></h1>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis enim esse eum, fuga fugit impedit iusto libero magnam perferendis perspiciatis repellendus similique! Aperiam dicta dolorem, harum molestiae soluta voluptate!
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci animi, dignissimos dolore eos est inventore ipsam ipsum labore, magnam nihil obcaecati, pariatur perferendis saepe sint totam voluptas! Alias, quo?
