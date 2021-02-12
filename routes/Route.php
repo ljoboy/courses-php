1d2747a2bee5670e1fd4b40f87a09f2254edfbd0
Dark Angel <jonathanyombo@gmail.com>
2021-02-12 10:55:41 +0200

    [NEW] Route class

---
+++ routes/Route.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * FILE ${FILE_NAME}
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 2/12/2021 - 10:49 AM
+ *
+ * @package Router;
+ */
+
+namespace Router;
+
+
+/**
+ * Class Route
+ * @package Router
+ */
+class Route
+{
+
+    /**
+     * @var string
+     */
+    public string $path;
+    /**
+     * @var string
+     */
+    public string $action;
+
+    /**
+     * Route constructor.
+     * @param string $path
+     * @param string $action
+     */
+    public function __construct(string $path, string $action)
+    {
+        $this->path = $path;
+        $this->action = $action;
+    }
+}
