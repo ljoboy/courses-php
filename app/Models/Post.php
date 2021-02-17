f5c14d501e53f95e47fa55c42a06337582a7e809
Dark Angel <jonathanyombo@gmail.com>
2021-02-17 22:53:13 +0200

    [NEW] add Post class

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * FILE Post.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 2/17/2021 - 7:14 PM
+ *
+ * @package App\Models;
+ */
+
+namespace App\Models;
+
+
+class Post extends Model
+{
+    public string $table = "posts";
+}
