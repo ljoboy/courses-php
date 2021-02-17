ca7c0573e3ae2c03b607c25d1d243694340adc51
Dark Angel <jonathanyombo@gmail.com>
2021-02-17 15:03:03 +0200

    get env variables from .env file

--- public/index.php
+++ public/index.php
@@ -9,14 +9,18 @@
 require '../vendor/autoload.php';
 
 use App\Controllers\BlogController;
+use Helper\DotEnv;
 use Router\Router;
 
 define('VIEWS', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 define('ASSETS', $_SERVER['SCRIPT_NAME'] . DIRECTORY_SEPARATOR);
 
+(new DotEnv(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '.env'))->load();
+
 $router = new Router();
 
-$router->get('/', [BlogController::class, 'index']);
-$router->get('/posts/:id', [BlogController::class, 'show']);
+$router->get('/', [BlogController::class, 'home']);
+$router->get('/posts', [BlogController::class, 'index']);
+$router->get('/post/:id', [BlogController::class, 'show']);
 
 $router->run();
