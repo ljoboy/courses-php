0861048a6431402b14cd9f3c80bdba248361cdfc
Dark Angel <jonathanyombo@gmail.com>
2021-02-17 22:45:04 +0200

    [UPDATE] add method to get all data from db

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -19,9 +19,36 @@ use PDO;
  */
 abstract class Model
 {
-    protected PDO $db;
+    /**
+     * @var PDO
+     */
+    private PDO $db;
+    /**
+     * The name of the table
+     *
+     * @var string
+     */
+    protected string $table;
+
+    /**
+     * Model constructor.
+     */
     public function __construct()
     {
         $this->db = (new DBConnection())->getPDO();
+    }
+
+    /**
+     * get all element from the db
+     *
+     * @param string $order_by
+     * @param string $order_format
+     * @return array
+     */
+    public function all(string $order_by = 'id', string $order_format = 'DESC'): array
+    {
+        $stmt = $this->db->query("SELECT * FROM {$this->table} ORDER BY {$order_by} {$order_format}");
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_class($this));
+        return $stmt->fetchAll();
     }
 }
